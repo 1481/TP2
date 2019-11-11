f9715771296003abebfdaac98e380ec8040a16f6
JacobLinCool <jacoblin2011@gmail.com>
2019-11-12 02:24:56 +0800

    Update c002.js

--- c/002/c002.js
+++ c/002/c002.js
@@ -1,19 +1,20 @@
-var c002 = function() {
+var c002 = function(t=1) {
     var input = "";
-    var a = Math.floor(-100+Math.random()*200);
-    var b = Math.floor(1+Math.random()*100);
-    var c = Math.floor(1+Math.random()*100);
-    input += a+" "+b+" "+c+"\n";
-    var skill = a;
-    for(var i = 0; i < b; i++) {
-        let x = Math.floor(Math.random()*10);
-        let y = Math.floor(1+Math.random()*20);
-        skill += x*c+11-y;
-        input += x+" "+y+"\n";
-    }
     var output = "";
-    output += skill+"\n";
-    
+    while(t--) {
+        let a = Math.floor(-100+Math.random()*200);
+        let b = Math.floor(1+Math.random()*100);
+        let c = Math.floor(1+Math.random()*100);
+        input += a+" "+b+" "+c+"\n";
+        let skill = a;
+        for(var i = 0; i < b; i++) {
+            let x = Math.floor(Math.random()*10);
+            let y = Math.floor(1+Math.random()*20);
+            skill += x*c+11-y;
+            input += x+" "+y+"\n";
+        }
+        output += skill+"\n";
+    }
     return {
         input: input,
         output: output
